77b8c323b190bc6c7811959af7867e3e2697ad15
Robert Bledsaw III <rbledsaw3@outlook.com>
2024-10-30 12:26:32 -0400

    Add game clock and fps clamp

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,6 +15,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(FPS)/1000
 
 
 if __name__ == "__main__":
